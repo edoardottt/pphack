060bde73acf5f1567a057b5452cffe12f71d550d
edoardottt <edoardott@gmail.com>
2024-02-18 08:35:37 +0100

    linting

--- pkg/output/json_test.go
+++ pkg/output/json_test.go
@@ -21,25 +21,35 @@ func TestFormatJSON(t *testing.T) {
 	}{
 		{name: "no error",
 			input: output.JSONData{
-				URL:          "https://edoardottt.github.io/pp-test?constructor.prototype.oigczu=oigczu\u0026__proto__[oigczu]=oigczu\u0026constructor[prototype][oigczu]=oigczu\u0026__proto__.oigczu=oigczu\u0026__proto__.oigczu=1|2|3\u0026__proto__[oigczu]={\"json\":\"value\"}#__proto__[oigczu]=oigczu",
+				URL: "https://edoardottt.github.io/pp-test?constructor.prototype.oigczu=oigczu\u0026__proto__[oigczu]=" +
+					"oigczu\u0026constructor[prototype][oigczu]=oigczu\u0026__proto__.oigczu=oigczu\u0026__proto__." +
+					"oigczu=1|2|3\u0026__proto__[oigczu]={\"json\":\"value\"}#__proto__[oigczu]=oigczu",
 				JSEvaluation: "oigczu",
 				Error:        "",
 			},
-			want: `{"URL":"https://edoardottt.github.io/pp-test?constructor.prototype.oigczu=oigczu\u0026__proto__[oigczu]=oigczu\u0026constructor[prototype][oigczu]=oigczu\u0026__proto__.oigczu=oigczu\u0026__proto__.oigczu=1|2|3\u0026__proto__[oigczu]={\"json\":\"value\"}#__proto__[oigczu]=oigczu","JSEvaluation":"oigczu"}`, //nolint:lll
+			want: `{"URL":"https://edoardottt.github.io/pp-test?constructor.prototype.oigczu=oigczu\u0026__proto__` +
+				`[oigczu]=oigczu\u0026constructor[prototype][oigczu]=oigczu\u0026__proto__.oigczu=oigczu\u0026__proto__.` +
+				`oigczu=1|2|3\u0026__proto__[oigczu]={\"json\":\"value\"}#__proto__[oigczu]=oigczu","JSEvaluation":"oigczu"}`,
 		},
 		{name: "error",
 			input: output.JSONData{
-				URL:          "https://edoardottt.github.io/pp-tes?constructor.prototype.sqtiwx=sqtiwx\u0026__proto__[sqtiwx]=sqtiwx\u0026constructor[prototype][sqtiwx]=sqtiwx\u0026__proto__.sqtiwx=sqtiwx\u0026__proto__.sqtiwx=1|2|3\u0026__proto__[sqtiwx]={\"json\":\"value\"}#__proto__[sqtiwx]=sqtiwx",
+				URL: "https://edoardottt.github.io/pp-tes?constructor.prototype.sqtiwx=sqtiwx\u0026__proto__[sqtiwx]" +
+					"=sqtiwx\u0026constructor[prototype][sqtiwx]=sqtiwx\u0026__proto__.sqtiwx=sqtiwx\u0026__proto__." +
+					"sqtiwx=1|2|3\u0026__proto__[sqtiwx]={\"json\":\"value\"}#__proto__[sqtiwx]=sqtiwx",
 				JSEvaluation: "",
 				Error:        "encountered an undefined value",
 			},
-			want: `{"URL":"https://edoardottt.github.io/pp-tes?constructor.prototype.sqtiwx=sqtiwx\u0026__proto__[sqtiwx]=sqtiwx\u0026constructor[prototype][sqtiwx]=sqtiwx\u0026__proto__.sqtiwx=sqtiwx\u0026__proto__.sqtiwx=1|2|3\u0026__proto__[sqtiwx]={\"json\":\"value\"}#__proto__[sqtiwx]=sqtiwx","Error":"encountered an undefined value"}`, //nolint:lll
+			want: `{"URL":"https://edoardottt.github.io/pp-tes?constructor.prototype.sqtiwx=sqtiwx\u0026__proto__[sqtiwx]` +
+				`=sqtiwx\u0026constructor[prototype][sqtiwx]=sqtiwx\u0026__proto__.sqtiwx=sqtiwx\u0026__proto__.` +
+				`sqtiwx=1|2|3\u0026__proto__[sqtiwx]={\"json\":\"value\"}#__proto__[sqtiwx]=` +
+				`sqtiwx","Error":"encountered an undefined value"}`,
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got, _ := output.FormatJSON(tt.input.URL, tt.input.JSEvaluation, tt.input.Error); !reflect.DeepEqual(string(got), tt.want) { //nolint:lll
+			got, _ := output.FormatJSON(tt.input.URL, tt.input.JSEvaluation, tt.input.Error)
+			if !reflect.DeepEqual(string(got), tt.want) {
 				t.Errorf("GetJSONString\n%v", string(got))
 				t.Errorf("want\n%v", tt.want)
 			}
